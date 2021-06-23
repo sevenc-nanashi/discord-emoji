adec4781f44d97a94680b9ae8c57aee3b1092662
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-06-23 22:55:38 +0900

    Add: Add aliases

--- discord_emoji/__init__.py
+++ discord_emoji/__init__.py
@@ -1,3 +1,7 @@
-from .methods import discord_to_unicode, unicode_to_discord  # noqa
+from .methods import to_unicode, to_discord, to_unicode_multi, to_discord_multi  # noqa
+discord_to_unicode = to_unicode
+unicode_to_discord = to_discord
+discord_to_uni = to_unicode
+unicode_to_dis = to_discord
 
 __version__ = "1.2.0"
