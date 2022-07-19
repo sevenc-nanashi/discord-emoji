e3cbd4c83d4c26fb26d881b36814d7a63cd2abd0
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-07-19 23:16:37 +0900

    Add: Add aliases

--- discord_emoji/__init__.py
+++ discord_emoji/__init__.py
@@ -6,4 +6,10 @@ unicode_to_dis = to_discord
 to_uni = to_unicode
 to_dis = to_discord
 
-__version__ = "1.4.0"
+__all__ = (
+    'to_unicode', 'to_discord', 'to_unicode_multi', 'to_discord_multi',
+    'discord_to_unicode', 'unicode_to_discord', 'discord_to_uni', 'unicode_to_dis',
+    'to_uni', 'to_dis'
+)
+
+__version__ = "1.4.1"
